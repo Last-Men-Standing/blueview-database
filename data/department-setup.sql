e22d08bc57e70c691a53850da331ff7fb2fbae3c
Joey Cengel <t80088@gmail.com>
2020-03-26 14:58:31 -0400

    Tested and functioning import and auto generated IDs for departments More specifically * Changed copy from STDIN to insert statements for ease * Fixed identity syntax to align with POSTGRES

--- data/department-setup.sql
+++ data/department-setup.sql
@@ -1,73 +1,74 @@
-COPY department (name, address, zipcode, overall_rating) FROM stdin;
-"Albany County Sheriff\'s Department"		"16 Eagle St, Albany, NY"					12207	5
-"Albany Police Department"                  "165 Henry Johnson Blvd, Albany, NY"		12210	5
-"Colonie Police Department"					"312 Wolf Road, Latham, NY"					12110	5
-"Guilderland Police Department"				"5209 Western Turnpike, Guilderland, NY"	12084	5
-"Watervliet Police Department"				"2 15th St Watervliet, NY"					12189	5
-"Cohoes Police Department"					"97 Mohawk St, Cohoes, NY"					12047	5
-"Altamont Police Department"				"115 Main St, Altamont, NY"					12009	5
-"Menands Police Department"					"250 Broadway, Menands, NY"					12204	5
-"Ravena Police Department"					"18 Russell Ave, Ravena, NY"				12143	5
-"Green Island Police Department"			"73 George St, Green Island, NY"			12183	5
+INSERT INTO department (name, address, zipcode, overall_rating)
+VALUES
+('Albany County Sheriffs Department','16 Eagle St, Albany, NY',					12207,	5),
+('Albany Police Department','165 Henry Johnson Blvd, Albany, NY',				12210,	5),
+('Colonie Police Department','312 Wolf Road, Latham, NY',						12110,	5),
+('Guilderland Police Department','5209 Western Turnpike, Guilderland, NY',		12084,	5),
+('Watervliet Police Department','2 15th St Watervliet, NY',						12189,	5),
+('Cohoes Police Department','97 Mohawk St, Cohoes, NY',							12047,	5),
+('Altamont Police Department','115 Main St, Altamont, NY',						12009,	5),
+('Menands Police Department','250 Broadway, Menands, NY',						12204,	5),
+('Ravena Police Department','18 Russell Ave, Ravena, NY',						12143,	5),
+('Green Island Police Department','73 George St, Green Island, NY',				12183,	5),
 
-"Columbia County Sheriff\'s Office"			"85 Industrial Tract, Hudson, NY"			12534	5
-"Hudson Police Department"					"701a Union St, Hudson, NY"					12534	5
-"Chatham Police Department"					"77 Main St, Chatham, NY"					12037	5
-"Philmont Police Department"				"Maple Ave, Philmont, NY"					12565	5
+('Columbia County Sheriffs Office','85 Industrial Tract, Hudson, NY',			12534,	5),
+('Hudson Police Department','701a Union St, Hudson, NY',						12534,	5),
+('Chatham Police Department','77 Main St, Chatham, NY',							12037,	5),
+('Philmont Police Department','Maple Ave, Philmont, NY',						12565,	5),
 
-"Fulton Police Department"					"141 S 1st St, Fulton, NY"					13069	5
-"Fulton County Sheriff\'s Department"		"2712 NY-29, Johnstown, NY"					12095	5
-"Gloversville Police Department"			"3 Frontage Rd, Gloversville, NY"			12078	5
-"Johnstown Police Department"				"41 E Main St, Johnstown NY"				12095	5
-"Northville Police Department"				"100 S 2nd St, Northville, NY"				12134	5
+('Fulton Police Department','141 S 1st St, Fulton, NY',							13069,	5),
+('Fulton County Sheriffs Department','2712 NY-29, Johnstown, NY',				12095,	5),
+('Gloversville Police Department','3 Frontage Rd, Gloversville, NY',			12078,	5),
+('Johnstown Police Department','41 E Main St, Johnstown NY',					12095,	5),
+('Northville Police Department','100 S 2nd St, Northville, NY',					12134,	5),
 
-"Greene Police Department"					"49 Genesee St, Greene, NY"					13778	5
-"Athens Police Department"					"2 1st St #1, Athens, NY"					12015	5
-"Cairo Police Department"					"16 Railroad Ave, Cairo, NY"				12413	5
-"Catskill Police Department"				"422 Main St, Catskill, NY"					12414	5
-"Coxsackie Police Department"				"119 Mansion St, Coxsackie, NY"				12051	5
-"Durham Police Department"					"7309 NY-81, East Durham, NY"				12423	5
-"Hunter Police Department"					"Tannersville, NY"							12485	5
-"Prattsville Police Department"				"14517 Main St, Prattsville, NY"			12468	5
-"Windham Police Department"					"371 NY-296, Hensonville, NY"			 	12439	5
+('Greene Police Department','49 Genesee St, Greene, NY',						13778,	5),
+('Athens Police Department','2 1st St #1, Athens, NY',							12015,	5),
+('Cairo Police Department','16 Railroad Ave, Cairo, NY',						12413,	5),
+('Catskill Police Department','422 Main St, Catskill, NY',						12414,	5),
+('Coxsackie Police Department','119 Mansion St, Coxsackie, NY',					12051,	5),
+('Durham Police Department','7309 NY-81, East Durham, NY',						12423,	5),
+('Hunter Police Department','Tannersville, NY',									12485,	5),
+('Prattsville Police Department','14517 Main St, Prattsville, NY',				12468,	5),
+('Windham Police Department','371 NY-296, Hensonville, NY',						12439,	5),
 
-"Montgomery County Sheriff\'s Office"		"200 Clark Dr, Fultonville, NY"				12072	5
-"Amsterdam Police Department"				"1 Guy Park Ext, Amsterdam, NY"				12010	5
-"Canajoharie Police Department"				"75 Erie Blvd, Canajoharie, NY"				13317	5
-"Fort Plains Police Department"				"168 Canal St, Fort Plain, NY"				13339	5
+('Montgomery County Sheriffs Office','200 Clark Dr, Fultonville, NY',			12072,	5),
+('Amsterdam Police Department','1 Guy Park Ext, Amsterdam, NY',					12010,	5),
+('Canajoharie Police Department','75 Erie Blvd, Canajoharie, NY',				13317,	5),
+('Fort Plains Police Department','168 Canal St, Fort Plain, NY',				13339,	5),
 
-"Rensselaer Police Department"				"201 Broadway, Rensselaer, NY"				12144	5
-"Rensselaer County Sheriff\'s Department"	"4000 Main St, Troy, NY"					12180	5
-"New Berlin Police Department"				"30 N Main St, New Berlin, NY"				13411	5
-"East Greenbush Police Department"			"225 Columbia Turnpike Rensselaer, NY"		12144	5
-"Hoosick Falls Police Department"			"24 Main St Hoosick Falls, NY"				12090	5
-"Troy Police Department"					"55 State St, Troy, NY"						12180	5
+('Rensselaer Police Department','201 Broadway, Rensselaer, NY',					12144,	5),
+('Rensselaer County Sheriffs Department','4000 Main St, Troy, NY',				12180,	5),
+('New Berlin Police Department','30 N Main St, New Berlin, NY',					13411,	5),
+('East Greenbush Police Department','225 Columbia Turnpike Rensselaer, NY',		12144,	5),
+('Hoosick Falls Police Department','24 Main St Hoosick Falls, NY',				12090,	5),
+('Troy Police Department','55 State St, Troy, NY',								12180,	5),
 
-"Saratoga County Sheriff\'s Office"			"6010 County Farm Rd, Ballston Spa, NY" 	12020	5
-"Ballston Spa Police Department"			"30 Bath St, Ballston Spa, NY"				12020	5
-"Mechanicville Police Department"			"36 N Main St #1, Mechanicville, NY"		12118	5
-"Saratoga Springs Police Department"		"5 Lake Ave, Saratoga Springs, NY"			12866	5
-"South Glens Falls Police Department"		"5 W Marion Ave, South Glens Falls, NY"		12803	5
-"Stillwater Police Department"				"1 Lansing Rd, Stillwater, NY"				12170	5
-"Waterford Police Department"				"65 Broad St, Waterford, NY"				12188	5
+('Saratoga County Sheriffs Office','6010 County Farm Rd, Ballston Spa, NY',		12020,	5),
+('Ballston Spa Police Department','30 Bath St, Ballston Spa, NY',				12020,	5),
+('Mechanicville Police Department','36 N Main St #1, Mechanicville, NY',		12118,	5),
+('Saratoga Springs Police Department','5 Lake Ave, Saratoga Springs, NY',		12866,	5),
+('South Glens Falls Police Department','5 W Marion Ave, South Glens Falls, NY',	12803,	5),
+('Stillwater Police Department','1 Lansing Rd, Stillwater, NY',					12170,	5),
+('Waterford Police Department','65 Broad St, Waterford, NY',					12188,	5),
 
-"Schenectady Police Department"				"531 Liberty St, Schenectady, NY"			12305	5
-"Schenectady Sheriff's Office"				"320 Veeder Ave, Schenectady, NY"			12307	5
+('Schenectady Police Department','531 Liberty St, Schenectady, NY',				12305,	5),
+('Schenectady Sheriffs Office','320 Veeder Ave, Schenectady, NY',				12307,	5),
 
-"Schoharie Police Department"				"256 Main St, Schoharie, NY"				12157	5
-"Schoharie County Sheriff\'s Office"		"157 Depot Ln, Schoharie, NY"				12157	5
-"Cobleskill Police Department"				"378 Mineral Springs Rd, Cobleskill, NY"	12043	5
+('Schoharie Police Department','256 Main St, Schoharie, NY',					12157,	5),
+('Schoharie County Sheriffs Office','157 Depot Ln, Schoharie, NY',				12157,	5),
+('Cobleskill Police Department','378 Mineral Springs Rd, Cobleskill, NY',		12043,	5),
 
-"Warren County Sheriff's Office"			"1400 U.S. 9, Lake George, NY"				12845	5
-"Glens Falls Police Department"				"42 Ridge St, Glens Falls, NY"				12801	5
-"South Glens Falls Police Department"		"5 W Marion Ave, South Glens Falls, NY"		12803	5
-"Lake George Peace Officers"				"32 Beach Rd, Lake George, NY"				12845	5
+('Warren County Sheriffs Office','1400 U.S. 9, Lake George, NY',				12845,	5),
+('Glens Falls Police Department','42 Ridge St, Glens Falls, NY',				12801,	5),
+('South Glens Falls Police Department','5 W Marion Ave, South Glens Falls, NY',	12803,	5),
+('Lake George Peace Officers','32 Beach Rd, Lake George, NY',					12845,	5),
 
-"Cambridge Police Department"				"56 N Park St, Cambridge, NY" 				12816	5
-"Fort Edward Police Department"				"118 Broadway, Fort Edward, NY"				12828	5
-"Granville Police Department"				"51 Quaker St, Granville, NY" 				12832	5
-"Greenwich Police Department"				"6 Academy St, Greenwich, NY" 				12834	5
-"New Hartford Police Department"			"8635 Clinton St, New Hartford, NY"			13413	5
-"Hudson Falls Police Department"			"218 Main St, Hudson Falls, NY"				12839	5
-"Whitehall Police Department"				"57 Skenesborough Dr, Whitehall, NY"		12887	5
-"Washington County Sheriff\'s Office"		"399 Broadway, Fort Edward, NY"				12828	5
+('Cambridge Police Department','56 N Park St, Cambridge, NY',					12816,	5),
+('Fort Edward Police Department','118 Broadway, Fort Edward, NY',				12828,	5),
+('Granville Police Department','51 Quaker St, Granville, NY',					12832,	5),
+('Greenwich Police Department','6 Academy St, Greenwich, NY',					12834,	5),
+('New Hartford Police Department','8635 Clinton St, New Hartford, NY',			13413,	5),
+('Hudson Falls Police Department','218 Main St, Hudson Falls, NY',				12839,	5),
+('Whitehall Police Department','57 Skenesborough Dr, Whitehall, NY',			12887,	5),
+('Washington County Sheriffs Office','399 Broadway, Fort Edward, NY',			12828,	5);
