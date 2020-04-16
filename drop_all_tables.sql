900ed3ba0738f09967f1bdc37360d1cadad01ecb
Richard Drum <richarddrum42@gmail.com>
2020-04-15 23:18:08 -0400

    added rating init with department setup

---
+++ drop_all_tables.sql
@@ -0,0 +1,6 @@
+DROP TABLE account;
+DROP TABLE department;
+DROP TABLE department_data;
+DROP TABLE post;
+DROP TABLE post_reply;
+DROP TABLE rating;
--- initialize.sql
+++ initialize.sql
@@ -1,3 +1,3 @@
 CREATE USER bv_user WITH PASSWORD 'alpine';
-CREATE DATABASE blueview_development;
+CREATE DATABASE blueview_development WITH OWNER bv_user;
 GRANT ALL PRIVILEGES ON DATABASE blueview_development TO bv_user;
